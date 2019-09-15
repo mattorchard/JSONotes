f2fe6e09962fd902d29ab98f2a1da0ce9b1bbb70
morch028 <morch028@uottawa.ca>
2019-09-15 14:33:00 -0400

    Switch file copying module and move watch and serve into rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,8 @@
 import babel from "rollup-plugin-babel";
 import { terser } from "rollup-plugin-terser";
-import copy from "rollup-plugin-copy"
-
+import copy from "rollup-plugin-copy-glob";
+import serve from "rollup-plugin-serve";
+import liveReload from "rollup-plugin-livereload";
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -13,12 +14,17 @@ export default {
     sourcemap: true
   },
   plugins: [
-    copy({
-      copyOnce: true,
-      hook: "writeBundle",
-      targets: [{src: ["src/*.html", "src/*.css"], dest: "public"}]
-    }),
-    production && babel({exclude: "node_modules/**"}),
-    production && terser() // minify, but only in production
+    copy([
+      { files: 'src/*.{html,css}', dest: 'public' },
+    ], { verbose: true, watch: true }),
+    ...(production
+      ? [
+        babel({exclude: "node_modules/**"}),
+        terser()
+      ]
+      : [
+        serve("./public"),
+        liveReload()
+      ])
   ]
 };
